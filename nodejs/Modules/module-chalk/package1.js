1033f8f81d4fae07578e9d380523d6c314b4d665
sahilgenius1 <sahilgenius1@gmail.com>
2020-09-04 06:16:42 +0530

    Add files via upload

---
+++ nodejs/Modules/module-chalk/package1.js
@@ -0,0 +1,9 @@
+const pi = 3.14;
+
+const areaRec = (l,b)=>l*b;
+const areaCirc = r => pi*r*r;
+const areaSq = a => a*a;
+
+module.exports={
+    areaRec,areaCirc,pi
+};
---
+++ nodejs/Modules/module-chalk/server.js
@@ -0,0 +1,7 @@
+const arithmetic =  require("./package1");
+const chalk = require("chalk");
+const arithemtic = require("./package1");
+
+console.log(chalk.red("I")+chalk.blue("am a developer"));
+
+console.log(chalk.green("area of circle is")+ arithemtic.areaCirc(5));
